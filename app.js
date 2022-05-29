68a0ebacf7cda09fc271460addbc5bdc122638de
PickleRick <bmkersey@gmail.com>
2022-05-28 22:42:51 -0400

    branch management

--- app.js
+++ app.js
@@ -147,9 +147,19 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err =>{
-        if (err) throw err;
-
-        console.log('Portfolio complete! Checkout out index.html to see the result!');
-    }); 
-});
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Page created! Check out index.html in this directory to see it!');
+        
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) {
+                console.log(err);
+                return;
+                }
+                console.log('Style sheet copied successfully!');
+            });
+        });
+    });
